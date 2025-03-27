99db045fd34c3c96fe5e545ef401957014121585
Sachin Sarkar <sachin.sarkar98@gmail.com>
2025-03-27 14:22:24 +0530

    feat: :sparkles: Initial commit

---
+++ src/main/java/com/hakerarena/railway/RailwayTicketCalendarInviteApplication.java
@@ -0,0 +1,13 @@
+package com.hakerarena.railway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayTicketCalendarInviteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayTicketCalendarInviteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hakerarena/railway/RailwayTicketCalendarInviteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakerarena.railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayTicketCalendarInviteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
