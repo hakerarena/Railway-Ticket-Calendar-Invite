f5dab1c1a5a691760f48bfb31110b702fde52272
Sachin Sarkar <sachin.sarkar98@gmail.com>
2025-04-16 12:57:18 +0530

    ci: :wrench: Fix configs

--- src/main/java/com/hakerarena/railway/config/CorsConfig.java
+++ src/main/java/com/hakerarena/railway/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.hakerarena.railway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,12 +11,11 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-            @SuppressWarnings("null")
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Apply to all endpoints
-                        .allowedOrigins("http://localhost:4200", "https://railway-ticket-calendar-invite.onrender.com")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://irctc-tkt-app.netlify.app")
+                        .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
